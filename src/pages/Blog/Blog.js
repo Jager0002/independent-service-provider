f72fc826d9d7c0b9d257f247ea9e10664623e00d
Jager0002 <faisalmoin8991@gmail.com>
2022-04-18 23:16:37 +0600

    Blog complete

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -2,8 +2,34 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div>
-      <p>Authentication Vs Authorization?</p>
+    <div className="m-8">
+      <p className="text-2xl">Authentication Vs Authorization?</p>
+      <p>
+        Authentication is basically the process of identifying the person. so
+        what it does is matches the user id and password and if it matches it
+        gives the access to the user.
+      </p>
+      <p>
+        Authorization takes place after the authentication is successfully
+        done.it gives the access to at what length the user can access the
+        pages. for example, certain premium memberships.
+      </p>
+
+      <p className="text-2xl">
+        Why are you using firebase?What other options do you have to implement
+        authentication?
+      </p>
+      <p>
+        Firebase is backed by google and is really known to people and also user
+        friendly UI. Other options are auth0, mongoDB, okta etc.
+      </p>
+      <p className="text-2xl">
+        What other services does firebase provide other than authentication?
+      </p>
+      <p>
+        Hosting, Storage, cloud Storage, Cloud Functions, Cloud Firestore, Cloud
+        Messaging, Predictions.
+      </p>
     </div>
   );
 };
